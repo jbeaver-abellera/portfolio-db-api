b276eed6d0ebc0fa8f59567734b55fdf91070a6a
jbeaver-abellera <abellera.beaver@gmail.com>
2024-09-27 00:55:26 +0800

    First Commit

---
+++ lambda_function.py
@@ -0,0 +1,61 @@
+import sys
+import logging as logger
+import psycopg2
+import json
+import os
+import boto3
+from datetime import datetime
+import pytz
+
+user_name = os.environ['USERNAME']
+password = os.environ['PASSWORD']
+rds_host = os.environ['RDS_HOST']
+rds_port = os.environ['RDS_PORT']
+db_name = os.environ['DB_NAME']
+
+    
+def lambda_handler(event, context):
+    items = []
+    # item_count = 0
+    
+    with conn.cursor() as curr:
+        table_name = 'view_counter'
+        #TO DO: get last data from table, as it is the latest timestamp
+        curr.execute(f"SELECT * FROM {table_name} ORDER BY id DESC LIMIT 1")
+        logger.info("The following items have been found in the db.")
+        last_row = curr.fetchone() #also the last row
+        row_id = last_row[0]
+        new_count = last_row[1] + 1
+        timestamp = get_timestamp()
+        
+        
+        #TO DO: create a new row with updated view and timestamp.
+        curr.execute(f"UPDATE {table_name} SET views={new_count}, timestamp=\'{timestamp}\' WHERE id={row_id}")
+        
+        conn.commit()
+        # Close the cursor and connection
+        curr.close()
+        conn.close()
+        
+        logger.info(f"New View Count: {new_count}")
+        return items
+
+def get_timestamp():
+    tz = pytz.timezone('Asia/Singapore')
+    return datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S %z")
+
+
+try:
+    conn = psycopg2.connect(host=rds_host, user=user_name, password=password, dbname=db_name, port=rds_port)
+
+    # ------------ Testing Only ------------------ #
+    event, context = '', ''
+    lambda_handler(event=event, context=context)
+    # -------------------------------------------- #
+    
+except psycopg2.Error as e:
+    logger.error("Unexpected error: Could not connect to Postgres instance.")
+    logger.error(e)
+    sys.exit
+
+logger.info("Success: Connection to RDS Postgres successful!")
