a995e4fd4e40a91a3659db2429aaca3e952002a4
jbeaver-abellera <abellera.beaver@gmail.com>
2024-09-28 00:38:02 +0800

    cleaning db connection

--- lambda_function.py
+++ lambda_function.py
@@ -18,27 +18,27 @@ def lambda_handler(event, context):
     items = []
     # item_count = 0
     
-    with conn.cursor() as curr:
+    with conn.cursor() as cur:
         table_name = 'view_counter'
         #TO DO: get last data from table, as it is the latest timestamp
-        curr.execute(f"SELECT * FROM {table_name} ORDER BY id DESC LIMIT 1")
+        cur.execute(f"SELECT * FROM {table_name} ORDER BY id DESC LIMIT 1")
         logger.info("The following items have been found in the db.")
-        last_row = curr.fetchone() #also the last row
+        last_row = cur.fetchone() #also the last row
         row_id = last_row[0]
         new_count = last_row[1] + 1
         timestamp = get_timestamp()
         
         
         #TO DO: create a new row with updated view and timestamp.
-        curr.execute(f"UPDATE {table_name} SET views={new_count}, timestamp=\'{timestamp}\' WHERE id={row_id}")
+        cur.execute(f"UPDATE {table_name} SET views={new_count}, timestamp=\'{timestamp}\' WHERE id={row_id}")
+        cur.close()
         
-        conn.commit()
-        # Close the cursor and connection
-        curr.close()
-        conn.close()
+    conn.commit()
+    # Close the cursor and connection
+    conn.close()
         
-        logger.info(f"New View Count: {new_count}")
-        return items
+    logger.info(f"New View Count: {new_count}")
+    return items
 
 def get_timestamp():
     tz = pytz.timezone('Asia/Singapore')
